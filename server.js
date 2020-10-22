e390c02e3a47cca071886ed273f68cae0620889d
jbrown7407 <jbrown7407@gmail.com>
2020-10-22 19:55:48 -0400

    x
    x

--- server.js
+++ server.js
@@ -23,9 +23,8 @@ app.use(methodOverride('_method'))
 // bring in the pokdex
 const pokedex = require('./models/pokedex.js')  //gives us access to the pokedex data in the models directory
 
-
 // ROUTES//
-// index
+// index //
 app.get('/pokedex/', (req, res) => {
   // res.send(pokedex)
   res.render('index.ejs', {
@@ -42,31 +41,41 @@ app.get('/pokedex/new', (req, res) => {
 
 //HOME INDEX
 app.get('/', (rec, res) => {
-    res.send('Welcome to the pokedex App!')
+    // res.send('Welcome to the pokedex App!')
+    res.redirect('/pokedex');
 })
 
 // CREATE NEW //
 app.post('/pokedex', (req, res) => {
-    console.log(req.body)
-    // if (req.body.readyToEat === 'on') { // if checked, req.body.readyToEat is set to 'on'
-    //   req.body.readyToEat = true
-    // } else { // if not checked, req.body.readyToEat is undefined
-    //   req.body.readyToEat = false
-    // }
-    pokedex.push(req.body)
-    res.redirect('/pokedex')
-  })
+  console.log(req.body)
+  // if (req.body.readyToEat === 'on') { // if checked, req.body.readyToEat is set to 'on'
+  //   req.body.readyToEat = true
+  // } else { // if not checked, req.body.readyToEat is undefined
+  //   req.body.readyToEat = false
+  // }
+  req.body.stats = {}
+  req.body.name = req.body.name
+  req.body.id = req.body.id
+  req.body.stats.speed = req.body.speed
+  req.body.stats.hp = req.body.hp
+  req.body.stats.attack = req.body.attack
+  req.body.stats.defense = req.body.defense
+  req.body.stats.spattack = req.body.spattack
+  req.body.stats.spdefense = req.body.spdefense
+  pokedex.push(req.body)
+  res.redirect('/pokedex')
+})
 
 //SHOW//    //ERROR HERE?
 app.get('/pokedex/:indexOfPokedexArray', (req, res) => {
   res.render('show.ejs', {
-    pokedex: pokedex[req.params.indexOfPokedexArray]
+    pokedex: pokedex[req.params.indexOfPokedexArray] // upper case? or allPokemon
   })
 })
 
-//DELETE
+//DELETE//
 app.delete('/pokedex/:indexOfPokedexArray', (req, res) => {
-  pokedex.splice(req.param.indexOfPokedex,1);
+  pokedex.splice(req.params.indexOfPokedexArray,1);
   res.redirect('/pokedex');
 })
 
@@ -82,18 +91,50 @@ app.get('/pokedex/:index/edit', (req, res)=>{
 })
 
 
-//UPDATE
-app.put('/pokedex/:index', (req, res) => { // :index is the index of our pokedex array that we want to change
-	// if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
-	// 	req.body.readyToEat = true
-	// } else { //if not checked, req.body.readyToEat is undefined
-	// 	req.body.readyToEat = false
-	// }
-	pokedex[req.params.index] = req.body //in our pokedex array, find the index that is specified in the url (:index).  Set that element to the value of req.body (the input data)
-	res.redirect('/pokedex'); //redirect to the index page
+//UPDATE//
+app.put('/pokedex/:indexOfPokedexArray', (req, res) => { // :index is the index of our pokedex array that we want to change
+	 if(req.body.captured === 'on'){ //if checked, req.body.captured is set to 'on'
+	 	req.body.captured = true
+	 } else { //if not checked, req.body.readyToEat is undefined
+	 	req.body.captured = false
+   }
+  //  console.log(pokedex[req.params.index])
+  console.log(req.body)
+	// pokedex[req.params.index] = req.body //in our pokedex array, find the index that is specified in the url (:index).  Set that element to the value of req.body (the input data)
+ ///
+  //  req.body.stats = {}
+  //  req.body.name = req.body.name
+  //  let name = req.body.name
+  //  req.body.id = req.body.id
+  //  req.body.stats.speed = req.body.speed
+  //  req.body.stats.hp = req.body.hp
+  //  req.body.stats.attack = req.body.attack
+  //  req.body.stats.defense = req.body.defense
+  //  req.body.stats.spattack = req.body.spattack
+  //  req.body.stats.spdefense = req.body.spdefense
+  ///
+  pokeman = {stats: {}}
+  pokeman.name = req.body.name
+  pokeman.id = req.body.id
+  pokeman.stats.speed = req.body.speed
+  pokeman.stats.hp = req.body.hp
+  pokeman.stats.attack = req.body.attack
+  pokeman.stats.defense = req.body.defense
+  pokeman.stats.spattack = req.body.spattack
+  pokeman.stats.spdefense = req.body.spdefense
+  console.log(req.params.indexOfPokedexArray)
+  pokedex[req.params.indexOfPokedexArray] = pokeman
+  // pokedex.push()
+  // pokedex.push(req.body)
+  res.redirect('/pokedex'); //redirect to the index page
 })
 
 // Listener //
 app.listen(PORT, () => {
   console.log('listening on port', PORT)
 })
+
+
+// <!-- <% for (let i = 0; i < pokemon.length; i++)  { %> 
+//   <a href="/pokedex/<%= i %> "><img id="shrinky" src="<%= pokedex[i].img %> "></a>
+// <% } %> -->   PRINT ONLY THE IMAGES
